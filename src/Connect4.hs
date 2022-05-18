8060f3467e1d3e4a65888759f72e8ad26d5ee232
Steven Chen <megakilo@gmail.com>
2022-05-17 22:09:50 -0700

    Use StateT monad the store the game states.

--- src/Connect4.hs
+++ src/Connect4.hs
@@ -1,18 +1,15 @@
 module Connect4 (startGame) where
 
-import Control.Monad (replicateM)
+import Control.Monad (guard)
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Control.Monad.Trans.State (StateT (StateT, runStateT), get, put)
 import Data.Char (isDigit)
-import Data.Foldable (find)
-import Data.Function (on)
-import Data.List (isInfixOf, sortBy, transpose)
-import Data.Maybe (fromMaybe)
+import Data.List (isInfixOf, transpose)
 import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
 import System.Process (system)
 
-startGame :: IO ()
-startGame = do
-  hSetBuffering stdout NoBuffering
-  game newGrid O
+-- Grid state and current player
+type GameState = (Grid, Player)
 
 -- Game configs --
 gridWidth = 7
@@ -37,56 +34,59 @@ type Matrix a = [Column a]
 type Grid = Matrix Player
 
 -- Game --
+startGame :: IO ()
+startGame = do
+  hSetBuffering stdout NoBuffering
+  runStateT playGame (newGrid, O)
+  return ()
+
+playGame :: StateT GameState IO ()
+playGame = do
+  game@(grid, _) <- get
+  liftIO $ system "clear"
+  liftIO $ putGrid grid
+  makeMove game
+  where
+    makeMove game@(grid, player)
+      | wins O grid = liftIO $ putStrLn "You win!"
+      | wins X grid = liftIO $ putStrLn "Computer wins!"
+      | player == O = do
+        i <- liftIO $ getUserInput grid
+        put $ addMove game i
+        playGame
+      | otherwise = do
+        liftIO $ putStrLn "Computer is thinking... "
+        put $! bestmove game
+        playGame
+
+getUserInput :: Grid -> IO Int
+getUserInput grid = do
+  putStr "Enter your move: "
+  xs <- getLine
+  case getColumn xs of
+    Just i -> return i
+    Nothing -> do
+      putStrLn "ERROR: Invalid move"
+      getUserInput grid
+  where
+    getColumn str = do
+      guard $ str /= [] && all isDigit str
+      let i = read str - 1
+      if valid grid i
+        then return i
+        else Nothing
+
 next :: Player -> Player
 next O = X
 next B = B
 next X = O
-
-game :: Grid -> Player -> IO ()
-game g p = do
-  system "clear"
-  putGrid g
-  play g p
-
-getNat :: String -> IO Int
-getNat prompt = do
-  putStr prompt
-  xs <- getLine
-  if xs /= [] && all isDigit xs
-    then return $ read xs - 1
-    else do
-      putStrLn "ERROR: Invalid move"
-      getNat prompt
-
-play :: Grid -> Player -> IO ()
-play g p
-  | wins O g = putStrLn "You win!\n"
-  | wins X g = putStrLn "Computer wins!\n"
-  | full g = putStrLn "It's a draw!\n"
-  | p == O = do
-    i <- getNat "Enter your move: "
-    case move g i p of
-      [] -> do
-        putStrLn "ERROR: Invalid move"
-        play g p
-      [g1] -> game g1 (next p)
-  | p == X = do
-    putStr "Computer is thinking... "
-    (game $! bestmove g p) (next p)
 
 -- Grid --
 newGrid :: Grid
 newGrid = replicate gridWidth (replicate gridHeight B)
 
 full :: Grid -> Bool
-full = notElem B . concat
-
-turn :: Grid -> Player
-turn g = if os <= xs then O else X
-  where
-    os = length (filter (== O) ps)
-    xs = length (filter (== X) ps)
-    ps = concat g
+full = notElem B . fmap head
 
 wins :: Player -> Grid -> Bool
 wins p g = any hasFourInRow (g ++ transpose g ++ dias)
@@ -100,17 +100,13 @@ won g = wins O g || wins X g
 valid :: Grid -> Int -> Bool
 valid g i = 0 <= i && i < gridWidth && head (g !! i) == B
 
-move :: Grid -> Int -> Player -> [Grid]
-move g i p =
-  if valid g i then [xs ++ [updated] ++ ys] else []
+addMove :: GameState -> Int -> GameState
+addMove (grid, player) i = (newGrid, next player)
   where
-    (xs, orig : ys) = splitAt i g
-    updated = addMove orig p
-
-addMove :: Column Player -> Player -> Column Player
-addMove col p = replicate (gridHeight - length current - 1) B ++ [p] ++ current
-  where
-    current = dropWhile (== B) col
+    (xs, orig : ys) = splitAt i grid
+    current = dropWhile (== B) orig
+    updated = replicate (gridHeight - length current - 1) B ++ player : current
+    newGrid = xs ++ [updated] ++ ys
 
 diag :: Show a => Matrix a -> [Column a]
 diag g = do
@@ -126,12 +122,8 @@ putGrid m = do
   putStrLn $ showIndex ++ "\n"
   where
     divider = replicate ((gridWidth * 4) + 1) '-'
-
-showRow :: Show a => [a] -> String
-showRow = concat . interleave "|" . map show
-
-showIndex :: String
-showIndex = concat $ interleave " " [" " ++ show i ++ " " | i <- [1 .. gridWidth]]
+    showIndex = concat $ interleave " " [" " ++ show i ++ " " | i <- [1 .. gridWidth]]
+    showRow = concat . interleave "|" . map show
 
 interleave :: a -> [a] -> [a]
 interleave x [] = [x]
@@ -141,33 +133,33 @@ interleave x (y : ys) = x : y : interleave x ys
 data Tree a = Node a [Tree a]
   deriving (Show)
 
-moves :: Grid -> Player -> [Grid]
-moves g p
-  | won g = []
-  | full g = []
-  | otherwise = concat [move g i p | i <- [0 .. gridWidth -1], valid g i]
+moves :: GameState -> [GameState]
+moves game@(grid, _)
+  | won grid = []
+  | full grid = []
+  | otherwise = [addMove game i | i <- [0 .. gridWidth -1], valid grid i]
 
-gametree :: Grid -> Player -> Tree Grid
-gametree g p = Node g [gametree g' (next p) | g' <- moves g p]
+gametree :: GameState -> Tree GameState
+gametree game = Node game [gametree g' | g' <- moves game]
 
 prune :: Int -> Tree a -> Tree a
 prune 0 (Node x _) = Node x []
 prune n (Node x ts) = Node x [prune (n -1) t | t <- ts]
 
-minimax :: Tree Grid -> Tree (Grid, Player)
-minimax (Node g [])
-  | wins O g = Node (g, O) []
-  | wins X g = Node (g, X) []
-  | otherwise = Node (g, B) []
-minimax (Node g ts)
-  | turn g == O = Node (g, minimum ps) ts'
-  | turn g == X = Node (g, maximum ps) ts'
+minimax :: Tree GameState -> Tree (GameState, Player)
+minimax (Node game@(grid, player) [])
+  | wins O grid = Node (game, O) []
+  | wins X grid = Node (game, X) []
+  | otherwise = Node (game, B) []
+minimax (Node game@(grid, player) ts)
+  | player == O = Node (game, minimum ps) ts'
+  | otherwise = Node (game, maximum ps) ts'
   where
     ts' = map minimax ts
     ps = [p | Node (_, p) _ <- ts']
 
-bestmove :: Grid -> Player -> Grid
-bestmove g p = head [g' | Node (g', p') _ <- ts, p' == best]
+bestmove :: GameState -> GameState
+bestmove gs = head [g' | Node (g', p') _ <- ts, p' == best]
   where
-    tree = prune gameTreeDepth (gametree g p)
+    tree = prune gameTreeDepth (gametree gs)
     Node (_, best) ts = minimax tree
